ecb3d07819ff5ab3721a4d50092140831a551dba
Ismael López <ismaellopez.dev@gmail.com>
2020-07-03 01:18:45 -0500

    :art: Added reset styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './container/App';
+import './assets/styles/App.scss';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
